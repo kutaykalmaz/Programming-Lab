a5f6c0ebcf77a7246f3d61328049cbe9cc1fd771
Kutay Kalmaz <kutaykalmaz@gmail.com>
2020-03-07 11:51:52 +0300

    Create 01.py

---
+++ Week02/01.py
@@ -0,0 +1,47 @@
+def max_of_two(a, b):
+    if a > b:
+        return a
+    return b
+
+
+def max_of_three(a, b, c):
+    return max_of_two(a, max_of_two(b, c))
+
+
+def findMaxRangeRecursive(list):
+    n = len(list)
+
+    if n == 1:
+        return list[0]
+
+    left_i = 0
+    left_j = n//2
+
+    right_i = n//2
+    right_j = n
+
+    left_sum = findMaxRangeRecursive(list[left_i:left_j])
+    right_sum = findMaxRangeRecursive(list[right_i:right_j])
+
+    temp_left_sum = 0
+    t = 0
+
+    for i in range(left_j-1, left_i-1, -1):
+        t = t + list[i]
+        if t > temp_left_sum:
+            temp_left_sum = t
+
+    t = 0
+    temp_right_sum = 0
+
+    for i in range(right_i,right_j):
+        t = t + list[i]
+        if t > temp_right_sum:
+            temp_right_sum = t
+
+    center_sum = temp_left_sum + temp_right_sum
+
+    return max_of_three(left_sum, right_sum, center_sum)
+
+list = [4, -3, 5, -2, -1, 2, 6, -2]
+print(findMaxRangeRecursive(list))
